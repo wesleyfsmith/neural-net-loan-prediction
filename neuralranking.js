8ef15f548698397afeabb7b1e444e4c1690b0e1b
wesley <wesleyfsmith@gmail.com>
2016-08-16 19:05:20 -0600

    Parsing data from text file.

--- neuralranking.js
+++ neuralranking.js
@@ -10,3 +10,22 @@ lr.on('error', function (err) {
 	// 'err' contains error object
   console.log(err);
 });
+
+var count = 0;
+
+lr.on('line', function (line) {
+
+  // console.log(count);
+
+	// 'line' contains the current line without the trailing newline character.
+
+  var result = line.split(' ').map(Number);
+
+  for (var i = 0; i < maxValues.length; i++) {
+
+    if (maxValues[i] < result[i]) {
+
+      maxValues[i] = result[i];
+
+    }
+  }
