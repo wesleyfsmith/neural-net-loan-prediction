1ff0d681b340646a625f244b92a2946911f92392
wesley <wesleyfsmith@gmail.com>
2016-08-25 23:35:31 -0600

    Added data normalization.

--- neuralranking.js
+++ neuralranking.js
@@ -39,3 +39,28 @@ lr.on('line', function (line) {
   count++;
 
 });
+
+lr.on('end', function () {
+
+	// All lines are read, file is closed now.
+
+  var testCases = [];
+
+
+
+  //normalize
+  for (var i = 0; i < data.length; i++) {
+
+    var row = data[i];
+
+    for (var j = 0; j < row.length; j++) {
+      row[j] = row[j] / maxValues[j];
+    }
+
+    data[i] = {input: data[i], output: defaults[i]};
+
+  }
+  //pull out some test data
+  for (var k = 100; k > 0; k--) {
+    testCases.push(data.pop());
+  }
